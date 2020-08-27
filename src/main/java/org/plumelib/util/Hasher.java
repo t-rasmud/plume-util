cfda1fd94f2d046bdb61ec4d8b4216998fd94633
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2020-08-26 22:18:37 -0700

    Intern cleanup

--- src/main/java/org/plumelib/util/Hasher.java
+++ src/main/java/org/plumelib/util/Hasher.java
@@ -1,5 +1,7 @@
 package org.plumelib.util;
 
+import org.checkerframework.checker.determinism.qual.*;
+
 /**
  * Hasher is intended to work like Comparable: it is an optional argument to a hashing data
  * structure (such as a HashSet, HashMap, or WeakHashMap) which specifies the hashCode() and
@@ -22,7 +24,7 @@ public interface Hasher {
    * @param o object whose hash code to compute
    * @return o's hash code
    */
-  int hashCode(Object o);
+  @NonDet int hashCode(Object o);
 
   /**
    * The equality function over the objects being hashed.
@@ -33,5 +35,5 @@ public interface Hasher {
    * @param o2 the second value to test for equality
    * @return whether o1 equals o2
    */
-  boolean equals(Object o1, Object o2);
+  @NonDet boolean equals(Object o1, Object o2);
 }
